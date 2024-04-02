67bf4df3d099ba73501b9f14f7ce89fce673dccb
Dan3Moshe <164073328+Dan3Moshe@users.noreply.github.com>
2024-04-02 20:52:19 +0300

    Add files via upload

---
+++ MosheDan_Vibestream I.sql
@@ -0,0 +1,180 @@
+﻿/*
+Question #1: 
+Return the percentage of users who have posted more than 10 times 
+rounded to 3 decimals.
+Expected column names: more_than_10_posts
+*/
+
+
+-- q1 solution:
+
+
+WITH question_one AS (
+  SELECT
+users.user_id AS userid,
+COUNT(posts.post_id) AS posts_count
+FROM users
+LEFT JOIN posts
+        ON users.user_id=posts.user_id
+
+
+GROUP BY userid
+)
+SELECT 
+  CAST(COUNT(CASE WHEN posts_count > 10 THEN 1 END)/CAST(COUNT(userid) AS float) AS DEC(4,3)) AS more_than_10_posts
+  FROM question_one;
+
+
+
+
+/*
+Question #2: 
+Recommend posts to user 888 by finding posts liked by users who have liked a post user 888 has also liked more than one time. 
+
+
+The output should adhere to the following requirements: 
+
+
+User 888 should not be recommend posts that they have already liked.
+Of the posts that meet the criteria above, return only the three most popular posts (by number of likes). 
+Return the post_ids in descending order.
+
+
+Expected column names: post_id
+*/
+
+
+-- q2 solution:
+
+
+WITH user888likes AS (
+    SELECT post_id
+    FROM likes
+    WHERE user_id = 888
+    GROUP BY post_id
+    
+)
+
+
+-- Find users who have liked the same posts as user 888
+, userswithsimilarlikes AS (
+    SELECT user_id
+    FROM likes
+    WHERE post_id IN (SELECT post_id FROM user888likes)
+    GROUP BY user_id
+    Having COUNT(*)>1
+)
+
+
+-- Find posts liked by users with similar likes, excluding posts liked by user 888
+, recommendedposts AS (
+    SELECT post_id, COUNT(user_id) AS like_count
+    FROM likes
+    WHERE user_id IN (SELECT user_id FROM userswithsimilarlikes) AND post_id NOT IN (SELECT post_id FROM User888Likes)
+    GROUP BY post_id
+)
+
+
+-- Rank the recommended posts by the number of likes
+, rankedposts AS (
+    SELECT post_id, like_count,
+           RANK() OVER (ORDER BY like_count DESC) AS rnk
+    FROM recommendedposts
+)
+
+
+-- Select the top 3 recommended posts
+SELECT post_id
+FROM rankedposts
+ORDER BY like_count DESC
+LIMIT 3;
+/*
+Question #3: 
+Vibestream wants to track engagement at the user level. When a user makes their first post, the team wants to begin tracking the cumulative sum of posts over time for the user.
+
+
+Return a table showing the date and the total number of posts user 888 has made to date. The time series should begin at the date of 888’s first post and end at the last available date in the posts table.
+
+
+Expected column names: post_date, posts_made
+*/
+
+
+-- q3 solution:
+
+
+-- Generate a series of dates from the first post date to the last available date in the posts table
+WITH dateseries AS (
+    SELECT generate_series(
+        ( SELECT MIN(post_date)
+           FROM posts
+           WHERE user_id = 888)::timestamp,
+        (SELECT MAX(post_date)
+         FROM posts )::timestamp,
+        interval '1 day'
+    )::date AS post_date
+)
+
+
+-- Count the cumulative sum of posts made by user 888 for each date
+, cumulativeposts AS (
+    SELECT
+        ds.post_date,
+        COUNT(p.post_id) OVER (ORDER BY ds.post_date) AS posts_made
+    FROM dateseries ds
+    LEFT JOIN posts p 
+  ON ds.post_date = p.post_date AND p.user_id = 888
+)
+
+
+-- Select the date and the total number of posts made by user 888 to date
+SELECT post_date, posts_made
+FROM cumulativeposts;
+
+
+/*
+Question #4: 
+The Vibestream feed algorithm updates with user preferences every day. Every update is independent of the previous update. Sometimes the update fails because Vibestreams systems are unreliable. 
+
+
+Write a query to return the update state for each continuous interval of days in the period from 2023-01-01 to 2023-12-30.
+
+
+the algo_update is 'failed' if tasks in a date interval failed and 'succeeded' if tasks in a date interval succeeded. every interval has a  start_dateand an end_date.
+
+
+Return the result in ascending order by start_date.
+
+
+Expected column names: algo_update, start_date, end_date
+*/
+
+
+-- q4 solution:
+
+
+WITH alldates AS (
+    SELECT fail_date AS update_date, 'failed' AS algo_update
+    FROM algo_update_failure
+    WHERE fail_date BETWEEN '2023-01-01' AND '2023-12-30'
+    UNION
+    SELECT success_date AS update_date, 'succeeded' AS algo_update
+    FROM algo_update_success
+    WHERE success_date BETWEEN '2023-01-01' AND '2023-12-30'
+)
+
+
+SELECT 
+    algo_update,
+    MIN(update_date) AS start_date,
+    MAX(update_date) AS end_date
+FROM (
+    SELECT 
+        update_date,
+        algo_update,
+        ROW_NUMBER() OVER (ORDER BY update_date) -
+        ROW_NUMBER() OVER (PARTITION BY algo_update ORDER BY update_date) AS grp
+    FROM alldates
+) AS grouped
+GROUP BY grp, algo_update
+ORDER BY start_date;
---
+++ MosheDan_traveltide.sql
@@ -0,0 +1,148 @@
+﻿/*
+Question #1: 
+Calculate the proportion of sessions abandoned in summer months 
+(June, July, August) and compare it to the proportion of sessions abandoned 
+in non-summer months. Round the output to 3 decimal places.
+
+
+Expected column names: summer_abandon_rate, other_abandon_rate
+*/
+
+
+SELECT
+  COUNT(DISTINCT(user_id)),
+  CAST(SUM(CASE WHEN EXTRACT(MONTH FROM session_start) IN (6, 7, 8) AND (hotel_booked = FALSE AND flight_booked = FALSE)  THEN 1 ELSE 0 END) / CAST(SUM(CASE WHEN EXTRACT(MONTH FROM session_start) IN (6, 7, 8) THEN 1 ELSE 0 END) AS float) AS DECIMAL(4, 3)) AS summer_abandon_rate,
+  CAST(SUM(CASE WHEN EXTRACT(MONTH FROM session_start) NOT IN (6, 7, 8) AND (hotel_booked = FALSE AND flight_booked = FALSE)  THEN 1 ELSE 0 END) / CAST(SUM(CASE WHEN EXTRACT(MONTH FROM session_start) NOT IN (6, 7, 8) THEN 1 ELSE 0 END) AS float) AS DECIMAL(4, 3)) AS other_abandon_rate
+FROM sessions;
+
+
+
+
+/*
+Question #2: 
+Bin customers according to their place in the session abandonment distribution as follows: 
+
+
+1. number of abandonments greater than one standard deviation more than the mean. Call these customers “gt”.
+2. number of abandonments fewer than one standard deviation less than the mean. Call these customers “lt”.
+3. everyone else (the middle of the distribution). Call these customers “middle”.
+
+
+calculate the number of customers in each group, the mean number of abandonments in each group, and the range of abandonments in each group.
+
+
+Expected column names: distribution_loc, abandon_n, abandon_avg, abandon_range
+
+
+*/
+
+
+WITH abandonment_stats AS (
+  SELECT
+        user_id,
+        SUM(CASE WHEN hotel_booked = FALSE AND flight_booked = FALSE THEN 1 ELSE 0 END) AS abandonments,
+        COUNT(user_id) AS total_sessions
+  FROM sessions
+  GROUP BY user_id
+  HAVING SUM(CASE WHEN hotel_booked = FALSE AND flight_booked = FALSE THEN 1 ELSE 0 END) != 0
+  ORDER BY abandonments DESC
+  )
+ 
+SELECT
+  CASE
+        WHEN abandonments > (SELECT AVG(abandonments) FROM abandonment_stats)  + (SELECT STDDEV(abandonments) FROM abandonment_stats) THEN 'gt'
+        WHEN abandonments < (SELECT AVG(abandonments) FROM abandonment_stats) - (SELECT STDDEV(abandonments) FROM abandonment_stats) THEN 'lt'
+        ELSE 'middle'
+  END AS distribution_loc,
+  COUNT(user_id) AS abandon_n,
+  AVG(abandonments) AS abandon_avg,
+  MAX(abandonments) - MIN(abandonments) AS abandon_range
+FROM abandonment_stats
+GROUP BY distribution_loc;
+
+
+
+
+/*
+Question #3: 
+Calculate the total number of abandoned sessions and the total number of sessions 
+that resulted in a booking per day, but only for customers who reside in one of the 
+top 5 cities (top 5 in terms of total number of users from city). 
+Also calculate the ratio of booked to abandoned for each day. 
+Return only the 5 most recent days in the dataset.
+
+
+Expected column names: session_date, abandoned,booked, book_abandon_ratio
+
+
+*/
+
+
+WITH TopCities AS (
+        SELECT home_city, COUNT(DISTINCT user_id) AS user_count
+        FROM users
+        GROUP BY home_city
+        ORDER BY user_count DESC
+        LIMIT 5
+)
+ 
+SELECT
+        date(session_start) as session_date,
+        SUM(CASE WHEN hotel_booked = FALSE AND flight_booked = FALSE THEN 1 ELSE 0 END) AS abandoned,
+        SUM(CASE WHEN hotel_booked = TRUE OR flight_booked = TRUE THEN 1 ELSE 0 END) AS booked,
+        CASE
+            WHEN SUM(CASE WHEN hotel_booked = FALSE AND flight_booked = FALSE THEN 1 ELSE 0 END) > 0
+            THEN CAST(SUM(CASE WHEN hotel_booked = TRUE OR flight_booked = TRUE THEN 1 ELSE 0 END) / CAST(SUM(CASE WHEN hotel_booked = FALSE AND flight_booked = FALSE THEN 1 ELSE 0 END) AS float) AS DEC(4,3))
+            ELSE 0
+        END AS book_abandon_ratio
+FROM sessions s
+JOIN users u ON s.user_id = u.user_id
+WHERE u.home_city IN (SELECT home_city FROM TopCities)
+GROUP BY session_date
+HAVING SUM(CASE WHEN hotel_booked = FALSE AND flight_booked = FALSE THEN 1 ELSE 0 END)>0
+ORDER BY session_date DESC
+LIMIT 5;
+
+
+/*
+Question #4: 
+Densely rank users from Saskatoon based on their ratio of successful bookings to abandoned bookings. 
+then count how many users share each rank, with the most common ranks listed first.
+
+
+note: if the ratio of bookings to abandons is null for a user, 
+use the average bookings/abandons ratio of all Saskatoon users.
+
+
+Expected column names: ba_rank, rank_count
+*/
+
+
+WITH saskatoonstats AS (
+        SELECT
+            s.user_id,
+            CASE
+            WHEN SUM(CASE WHEN hotel_booked = FALSE AND flight_booked = FALSE AND cancellation=FALSE THEN 1 ELSE 0 END) > 0
+            THEN CAST(SUM(CASE WHEN (hotel_booked = TRUE OR flight_booked = TRUE)AND cancellation=FALSE THEN 1 ELSE 0 END) / CAST(SUM(CASE WHEN hotel_booked = FALSE AND flight_booked = FALSE AND cancellation=FALSE THEN 1 ELSE 0 END) AS float) AS DEC(4,3))
+            ELSE NULL END AS booking_abandon_ratio
+        FROM sessions s
+        JOIN users u ON s.user_id = u.user_id
+        WHERE home_city = 'saskatoon'
+        GROUP BY s.user_id
+)
+, finalresult AS (
+        SELECT
+            user_id,
+            CASE
+                WHEN booking_abandon_ratio = NULL THEN AVG(booking_abandon_ratio) OVER ()
+                ELSE booking_abandon_ratio
+            END AS adjusted_booking_abandon_ratio
+        FROM saskatoonstats
+)
+ 
+SELECT
+        DENSE_RANK() OVER (ORDER BY adjusted_booking_abandon_ratio DESC) AS ba_rank,
+        COUNT(user_id) AS rank_count
+FROM finalresult
+GROUP BY adjusted_booking_abandon_ratio
+ORDER BY rank_count DESC, ba_rank ;
